dda01a9b10465711ed7ca13cffda2a4606e96b45
Artur Koza <artur.koza.developer@gmail.com>
2025-02-25 19:43:18 +0100

    temporary fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,32 @@
 import './styles/App.css'
 import {Background} from "./Background.tsx";
-import {useEffect, useState} from "react";
-import {FormattedMessage, IntlProvider} from "react-intl";
+// import {useEffect, useState} from "react";
+// import {FormattedMessage, IntlProvider} from "react-intl";
 
-const loadTranslation = async (locale: string) => {
-  try {
-    const response = await fetch(`/translations/${locale}.json`);
-    return await response.json();
-  } catch (error) {
-    console.error('Error loading translation:', error);
-    return {};
-  }
-};
+// const loadTranslation = async (locale: string) => {
+//   try {
+//     const response = await fetch(`/translations/${locale}.json`);
+//     return await response.json();
+//   } catch (error) {
+//     console.error('Error loading translation:', error);
+//     return {};
+//   }
+// };
 
 function App() {
-  const [locale, setLocale] = useState('pl');
-  const [messages, setMessages] = useState({});
+  // const [locale, setLocale] = useState('pl');
+  // const [messages, setMessages] = useState({});
 
-  useEffect(() => {
-    loadTranslation(locale).then((translations) => setMessages(translations));
-  }, [locale]);
+  // useEffect(() => {
+  //   loadTranslation(locale).then((translations) => setMessages(translations));
+  // }, [locale]);
 
   return (
   //     <div>
   // <IntlProvider locale={locale} messages={messages}>
-    <Background />
+    <Background>
+      <div></div>
+    </Background>
   //  <FormattedMessage id={"hello"} defaultMessage={"Default hello"}/>
   //   <select value={locale} onChange={(e) => setLocale(e.target.value)}>
   //     <option value="en">English</option>
