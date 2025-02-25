66d49e1b9e6393993db66003249a61c1f65481bd
Artur Koza <artur.koza.developer@gmail.com>
2025-02-25 13:34:03 +0100

    add translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
 import './App.css'
 import {Home} from "./Home.tsx";
+import {useEffect, useState} from "react";
+import {FormattedMessage, IntlProvider} from "react-intl";
+
+const loadTranslation = async (locale: string) => {
+  try {
+    const response = await fetch(`/translations/${locale}.json`);
+    return await response.json();
+  } catch (error) {
+    console.error('Error loading translation:', error);
+    return {};
+  }
+};
 
 function App() {
+  const [locale, setLocale] = useState('en');
+  const [messages, setMessages] = useState({});
+
+  useEffect(() => {
+    loadTranslation(locale).then((translations) => setMessages(translations));
+  }, [locale]);
 
   return (
+      <div>
+  <IntlProvider locale={locale} messages={messages}>
+   <FormattedMessage id={"hello"}/>
+    <select value={locale} onChange={(e) => setLocale(e.target.value)}>
+      <option value="en">English</option>
+      <option value="pl">Polish</option>
+    </select>
+  </IntlProvider>
      <Home />
+      </div>
   )
 }
 
