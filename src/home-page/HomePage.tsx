72c36da2002cf6b02c7628338cd2a2f83e41ee40
Artur Koza <artur.koza.developer@gmail.com>
2025-03-03 11:13:24 +0100

    some home page and menu fixes

--- src/home-page/HomePage.tsx
+++ src/home-page/HomePage.tsx
@@ -1,12 +1,15 @@
 import "../styles/HomePage.css"
 import {FormattedMessage} from "react-intl";
+import {PageContext} from "../context/PageContext.tsx";
+import {useContext} from "react";
 
 export const HomePage = () => {
+    const {showMenu} = useContext(PageContext);
     return(
         <div className="home-page">
             <div className={"home-header"}>
-            <div className={"page-title"}><FormattedMessage id={"page-title"} defaultMessage={"Artur Koza"}/></div>
-            {/*<div className={"page-subtitle"}><FormattedMessage id={"page-subtitle"} defaultMessage={"Conductor"}/></div>*/}
+                {!showMenu && <div className={"page-title"}><FormattedMessage id={"page-title"} defaultMessage={"Artur Koza"}/>
+                </div>}
             </div>
         </div>
     )
